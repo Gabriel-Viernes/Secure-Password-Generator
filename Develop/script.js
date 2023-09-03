536ca6478d42242c12e1de2c87369031b9459a53
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-03 14:29:01 -0500

    added initial password length query, console logs for debugging

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,29 @@
 // Assignment code here
+function getPassLength() {
+  let getLength = prompt("Please enter, in numbers, your desired password length. Length must be between 8 and 128 characters.");
+  let passLength = Number(getLength);
+  if(passLength < 8 || passLength > 128 || passLength===NaN) {
+    alert(`Invalid format, password length must be in numbers and between 8 and 128`);
+    return false;
+  } else {
+    return passLength;
+  }
+}
+
+function promptCritGen() {
+  if(getPassLength === false) {
+    return;
+  }
+
+}
+
+function generatePassword() {
+  console.log("Button clicked")
+  // prompt generator
+  console.log(`Password length selected: ${promptCritGen()}`);
+  
+}
+
 
 
 // Get references to the #generate element
