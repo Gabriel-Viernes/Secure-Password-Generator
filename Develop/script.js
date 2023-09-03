d8fb01d4adb53518f51121fa05292afdade76a5a
Gabriel Viernes <viernesgabriel@gmail.com>
2023-09-03 15:05:06 -0500

    added character type queries, password generation now cancels on invalid input

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,17 @@
 // Assignment code here
+function yesNoCheck(x) {
+  if((x === `Y`) || (x === `N`) || (x === `y`) || (x === `n`)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function getPassLength() {
   let getLength = prompt("Please enter, in numbers, your desired password length. Length must be between 8 and 128 characters.");
   let passLength = Number(getLength);
   if(passLength < 8 || passLength > 128 || passLength===NaN) {
-    alert(`Invalid format, password length must be in numbers and between 8 and 128`);
+    alert(`Invalid format, password length must be in numbers and between 8 and 128. Please generate another password.`);
     return false;
   } else {
     return passLength;
@@ -11,16 +19,42 @@ function getPassLength() {
 }
 
 function promptCritGen() {
-  if(getPassLength === false) {
+  let i = getPassLength();
+  console.log(`Chosen password length: ${i}`);
+  if(i === false) {
+    console.log(`Invalid length specified`)
     return;
   }
+  let lowerQuery = prompt("Would you like your password to include lowercase characters? Type Y/N");
+  console.log(`lowercase selection ${lowerQuery}`);
+  let upperQuery = prompt("Would you like your password to include uppercase characters? Type Y/N");
+  console.log(`uppercase selection ${upperQuery}`);
+  let numQuery = prompt("Would you like your password to include numbers? Type Y/N");
+  console.log(`number selection ${numQuery}`);
+  let specQuery = prompt("Would you like your password to include special characters? Type Y/N");
+  console.log(`special character selection ${specQuery}`);
+  let queryArr = [lowerQuery,upperQuery,numQuery,specQuery];
+  for (let i = 0; i < queryArr.length; i++) {
+    if(yesNoCheck(queryArr[i]) === false) {
 
+      console.log(`Invalid response detected, canceling...`);
+      return;
+    }
+  }
+  queryArr.push(i);
+  return queryArr;
 }
 
 function generatePassword() {
-  console.log("Button clicked")
+  console.log("Password generation initialized")
   // prompt generator
-  console.log(`Password length selected: ${promptCritGen()}`);
+  let queryArr = promptCritGen();
+  if(queryArr === undefined) {
+    console.log(`Password generation canceled`);
+    alert(`Invalid selection, please answer using Y/N only`)
+    return;
+  }
+
   
 }
 
